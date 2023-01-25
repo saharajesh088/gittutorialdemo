adbbed4418b4b9875c513f0212e29d71a0b34c14
Rajesh Saha <saharajesh599@gmail.com>
2023-01-25 19:03:51 +0530

    task 12

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 function saveToStorage(event){
     event.preventDefault();
+    let form = document.getElementById('form');
     let name = document.getElementById("text").value;
     let email = document.getElementById("email").value;
-    localStorage.setItem("name",name);
-    localStorage.setItem("email",email);
     let obj = {
         name:name,
         email:email
     }
     let objSerialization = JSON.stringify(obj);
-    localStorage.setItem("myObj",objSerialization);
-    let objDeserialization = JSON.parse(localStorage.getItem("myObj"));
+    localStorage.setItem(obj.email,objSerialization);
+
+    Object.keys(localStorage).forEach((key)=>{
+        console.log(JSON.parse(localStorage.getItem(key)));
+    });
+    userDetailsOnScreen(obj);
+
+}
+function userDetailsOnScreen(user){
+    let items = document.getElementById("listOfItems");
+    let childHtml = `<li> ${user.name} - ${user.email} </li>`;
+    items.innerHTML = items.innerHTML + childHtml;
+    items.appendChild(li);
+
 }
